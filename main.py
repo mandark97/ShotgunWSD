70509ba0de1234401796c924e7badc116c6a1bf6
Robert Iancu <robert.iancu@morecommerce.com>
2020-05-02 20:21:25 +0300

    Add evaluator for results

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ from relatedness.word_embedding import WordEmbeddingRelatedness
 from operations import AddOperation
 from parser import Parser
 from result_writer import ResultWriter
+from scorer import Scorer
 from shotgun_wsd import ShotgunWSD
 from synset_utils import SynsetUtils
 
-documents = Parser().run()
+
+DOCUMENTS_PATH = "datasets/semeval2013/semeval2013.data.xml"
+ANSWERS_PATH = "datasets/semeval2013/semeval2013.gold.key.txt"
+RESULTS_PATH = "results.txt"
+
+documents = Parser(filename=DOCUMENTS_PATH).run()
 logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
 document = documents[0]
@@ -21,4 +27,8 @@ shotgun_wsd = ShotgunWSD(document=document, window_size=2, number_configs=2, syn
                          min_synset_collision=2, max_synset_collision=4, number_of_votes=4)
 final_senses = shotgun_wsd.run()
 
-ResultWriter(document, final_senses).write("results.txt", mode=ResultWriter.SCORE)
+ResultWriter(document, final_senses).write(RESULTS_PATH, mode=ResultWriter.SCORE)
+
+scorer = Scorer(RESULTS_PATH, ANSWERS_PATH)
+scorer_results = scorer.score()
+print(scorer_results)
